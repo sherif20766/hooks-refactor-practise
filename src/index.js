e434800b0d12f19e6a01c169ae4aa28e19f41a3f
sherif20766 <sherif20766@hotmail.com>
2020-01-04 14:31:14 +0000

    refactored the App component to use hooks

--- src/index.js
+++ src/index.js
@@ -1,51 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 import Spinner from "./Spinner";
 
+const App = () => {
+	const [lat, setLat] = useState(null);
+	const [errorMessage, setErrorMessage] = useState("");
 
+	useEffect(() => {
+		window.navigator.geolocation.getCurrentPosition(
+			position => setLat(position.coords.latitude),
+			err => setErrorMessage(err.message)
+		);
+	}, []);
 
-class App extends React.Component {
+	let content;
 
-	state = { lat: null, errorMessage: "" }
-
-	componentDidMount() {
-	  window.navigator.geolocation.getCurrentPosition(
-	    position => this.setState({ lat: position.coords.latitude }) , 
-	    err => this.setState({ errorMessage: err.message })
-	  );
+	if (errorMessage) {
+		content = <div>Error: {errorMessage}</div>;
+	} else if (lat) {
+		content = <SeasonDisplay lat={lat} />;
+	} else {
+		content = (
+			<Spinner size="massive" message="Please accept location request" />
+		);
 	}
 
-	renderContent() {
-		if (this.state.errorMessage && !this.state.lat) {
-			return <div>Error: {this.state.errorMessage}</div>
-		}
-
-		if (!this.state.errorMessage && this.state.lat) {
-			return <SeasonDisplay lat={this.state.lat}/>
-		}
-		
-		return <Spinner 
-				size="massive" 
-				message="Please accept location request"
-			   />
-	}
-
-	render() {
-		
-		return (
-			<div>
-				{this.renderContent()}
-			</div>
-		)
-
-		
-	};
+	return <div className="border red">{content}</div>;
 };
 
-
-ReactDOM.render(
-  <App />,
-  document.querySelector("#root")
-);
-
+ReactDOM.render(<App />, document.querySelector("#root"));
